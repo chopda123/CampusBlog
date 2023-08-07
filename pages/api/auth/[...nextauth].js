eae77bdf01d0f43e79cccdc60bb457a2b6fdb1cb
yash bansod <ybbansod@mitaoe.ac.inm>
2023-08-07 14:21:12 +0530

    secret update

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -7,10 +7,11 @@ export const authOptions = {
   providers: [
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       })
     // ...add more providers here
   ],
+  secret: process.env.NEXTAUTH_SECRET,
 }
 
 export default NextAuth(authOptions)
